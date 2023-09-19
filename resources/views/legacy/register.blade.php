0d5303d1c79882f2d6f12af8fab95af073131581
alex <alex.heritier@gmail.com>
2023-09-19 14:53:56 +0700

    register screen css refactor

--- resources/views/legacy/register.blade.php
+++ resources/views/legacy/register.blade.php
@@ -6,8 +6,8 @@
 
 
 @section('script')
-    <script type="text/javascript">
-        function validateSize(input) {
+<script type="text/javascript">
+    function validateSize(input) {
             const file = input.files[0];
             const fileSize = file.size / 1024.0 / 1024.0; // in MiB
             const fileType = file.type;
@@ -44,7 +44,7 @@
 
             const el = document.getElementById('biz-form-section');
             if (checked) {
-                el.style.display = 'block';
+                el.style.display = 'contents';
             } else {
                 el.style.display = 'none';
             }
@@ -70,8 +70,8 @@
                 document.getElementById("existing-account-form").style.display = "none";
 
                 // Toggle button class
-                document.getElementById("register-button").classList.add('btn-on');
-                document.getElementById("existing-account-button").classList.remove('btn-on');
+                document.getElementById("register-button").classList.add('btn--active');
+                document.getElementById("existing-account-button").classList.remove('btn--active');
 
                 document.querySelector('p.alert').innerText = '';
             });
@@ -81,8 +81,8 @@
                 document.getElementById("existing-account-form").style.display = "block";
 
                 // Toggle button class
-                document.getElementById("register-button").classList.remove('btn-on');
-                document.getElementById("existing-account-button").classList.add('btn-on');
+                document.getElementById("register-button").classList.remove('btn--active');
+                document.getElementById("existing-account-button").classList.add('btn--active');
 
                 document.querySelector('p.alert').innerText = '';
             });
@@ -91,148 +91,133 @@
 
             onBizCheckChange(document.querySelector('input[type="checkbox"]').checked);
         });
-    </script>
+</script>
 @endsection
 
 
 @section('content')
-    <div id="sign-up-screen">
-        <div id="button-container">
-            {{-- <button id="register-button" class="{{ $regClass }}">{{ __('Register') }}</button>
-            <button id="existing-account-button" class="{{ $loginClass }}">{{ __('Sign in') }}</button> --}}
-            <button id="register-button" @class(['btn-on' => (session('err_type') === 'register' || session('err_type') === null)])>{{ __('Register') }}</button>
-            <button id="existing-account-button"  @class(['btn-on' => session('err_type') === 'login'])>{{ __('Sign in') }}</button>
-        </div>
-
-         @if($errors->count() > 0)
-            @foreach ($errors->all() as $error)
-                <p class="alert">{{ $error }}</p>
-            @endforeach
-        @endif
-        
-        <div id="main-content">
-            <div id="register-form">
-                <!-- Form elements for registering a new account -->
-                <form method="post" action="{{ route('register') }}" enctype="multipart/form-data">
-                    @csrf
-
-                    <!-- Account info -->
-                    <label for="email">{{ __('Email') }}</label>
-                    <input type="email" name="email" placeholder="{{ __('Enter email') }}" /><br/>
-                    <label for="password">{{ __('Password') }}</label>
-                    <input type="password" name="password" placeholder="{{ __('Create password') }}"/><br/>
-
-                    <br/>
-                    <div style="display: flex; gap: 10px">
-                        <input id="is-biz-checkbox" type="checkbox" onchange="onBizCheckChange(this.checked)">
-                        <label onclick="onCheckLabelClick()">{{ __('I am a professional tradesman') }}</label>
-                    </div>
-                    <br/>
-
-                    <!-- Business info -->
-                    <div id="biz-form-section" style="display: none">
-                        <label for="biz_name">{{ __('Business name') }}</label>
-                        <input type="text" name="biz_name" placeholder="{{ __('Business name') }}" /><br/>
-                        <div style="display: flex; gap: 8px;">
-                            <div>
-                                <label for="first_name">{{ __('First name') }}</label>
-                                <input type="text" name="first_name"  placeholder="{{ __('First name') }}" /><br/>
-                            </div>
-                            <div>
-                                <label for="last_name">{{ __('Last name') }}</label>
-                                <input type="text" name="last_name"  placeholder="{{ __('Last name') }}" /><br/>
-                            </div>
-                        </div>
-                        <label for="phone">{{ __('Phone number') }}</label>
-                        <div id="phone-fields">
-                            <select name="phone_code">
-                                <option value="1">🇺🇸 +1</option>
-                                <option value="84">🇻🇳 +84</option>
-                            </select>
-                            <input type="tel" name="phone"  placeholder="{{ __('Phone number') }}" />
-                        </div>
-                        <label for="descr">{{ __('Description') }}</label>
-                        <input type="text" name="descr"  placeholder="{{ __('Description') }}" /><br/>
-                        {{-- <label for="website">{{ __('Website') }}</label>
-                        <input type="text" name="website"  placeholder="{{ __('Website') }}" /><br/> --}}
-                        <label for="pricing">{{ __('Pricing').' ('.$currency_symbol.')' }}</label>
-                        <input type="number" name="pricing" step="0.01" placeholder="{{ __('Pricing') }}" /><br/>
-
-                        <!-- Business trade -->
-                        <label for="trade">{{ __('Trade') }}</label>
-                        <select name="trade">
-                            @foreach ($trades as $key => $value)
-                                <option value="{{ $key }}">{{ $value }}</option>
-                            @endforeach
-                            {{-- <option value="electrician">Electrician</option>
-                            <option value="plumber">Plumber</option>
-                            <option value="carpenter">Carpenter</option>
-                            <option value="hvac">HVAC technician</option>
-                            <option value="welder">Welder</option>
-                            <option value="mechanic">Mechanic</option>
-                            <option value="painter">Painter</option>
-                            <option value="roofer">Roofer</option>
-                            <option value="mason">Mason</option>
-                            <option value="landscaper">Landscaper</option>
-                            <option value="drywall">Drywall installer</option>
-                            <option value="insulator">Insulator</option>
-                            <option value="concrete">Concrete worker</option>
-                            <option value="glazier">Glazier</option>
-                            <option value="flooring">Flooring installer</option>
-                            <option value="sheetmetal">Sheet metal worker</option>
-                            <option value="bricklayer">Bricklayer</option>
-                            <option value="ironworker">Ironworker</option> --}}
-                        </select>
-                        <br/>
-
-                        <!-- Business location (loaded dynamically) -->
-                        <div id="loc-picker">
-                            <div>
-                                <label for="district">{{ __('District') }}</label>
-                                <select class="notranslate" name="district">
-                                    @foreach ($districts as $district)
-                                        <option value="{{ $district["code"] }}">{{ $district["name"] }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div>
-                                <label for="ward">{{ __('Ward') }}</label>
-                                <select class="notranslate" name="ward">
-                                    @foreach ($districts as $district)
-                                        @foreach ($district['ward'] as $ward)
-                                            <option 
-                                                class="{{ $district == $districts[0] ? '' : 'inactive' }}" 
-                                                x-district="{{ $district["code"] }}" 
-                                                value="{{ $ward["code"] }}">{{ $ward["name"] }}</option>
-                                        @endforeach
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                        <br/>
-
-                        <input onchange="validateSize(this)" type="file" name="image" />
-                        <p id="file-error">&nbsp;</p>
-                        <br/>
-                    </div>
-
-                    <input type="submit" value="{{ __('Submit') }}" />
-                </form>
-            </div>
-            <div id="existing-account-form" style="display:none">
-                <!-- Form elements for logging into an existing account -->
-                <form method="post" action="{{ route('login') }}">
-                    @csrf
-
-                    <label for="email">{{ __('Email') }}</label>
-                    <input type="email" name="email" placeholder="{{ __('Enter email') }}" /><br/>
-                    <label for="password">{{ __('Password') }}</label>
-                    <input type="password" id="password" name="password" placeholder="{{ __('Create password') }}" /><br/>
-                    
-                    <input type="submit" value="{{ __('Submit') }}" />
-                </form>
-            </div>
-        </div>
+<div id="sign-up-screen">
+    <div id="button-container">
+        {{-- <button id="register-button" class="{{ $regClass }}">{{ __('Register') }}</button>
+        <button id="existing-account-button" class="{{ $loginClass }}">{{ __('Sign in') }}</button> --}}
+        <button class="btn" id="register-button" @class(['btn--active'=> (session('err_type') === 'register' ||
+            session('err_type') ===
+            null)])>{{ __('Register') }}</button>
+        <button class="btn" id="existing-account-button" @class(['btn--active'=> session('err_type') === 'login'])>{{
+            __('Sign in')
+            }}</button>
     </div>
+
+    @if($errors->count() > 0)
+    @foreach ($errors->all() as $error)
+    <p class="alert">{{ $error }}</p>
+    @endforeach
+    @endif
+
+    <section class="solo-box" id="main-content">
+        <div id="register-form">
+            <!-- Form elements for registering a new account -->
+            <form class="form-24" method="post" action="{{ route('register') }}" enctype="multipart/form-data">
+                @csrf
+
+                <!-- Account info -->
+                <label for="email">{{ __('Email') }}</label>
+                <input type="email" name="email" placeholder="{{ __('Enter email') }}" />
+                <label for="password">{{ __('Password') }}</label>
+                <input type="password" name="password" placeholder="{{ __('Create password') }}" />
+
+
+                <div style="display: flex; gap: 10px">
+                    <input id="is-biz-checkbox" type="checkbox" onchange="onBizCheckChange(this.checked)">
+                    <label onclick="onCheckLabelClick()">{{ __('I am a professional tradesman') }}</label>
+                </div>
+
+
+                <!-- Business info -->
+                <div id="biz-form-section" style="display: none">
+                    <label for="biz_name">{{ __('Business name') }}</label>
+                    <input type="text" name="biz_name" placeholder="{{ __('Business name') }}" />
+                    <div class="form-24__row">
+                        <div class="form-24__entry">
+                            <label for="first_name">{{ __('First name') }}</label>
+                            <input type="text" name="first_name" placeholder="{{ __('First name') }}" />
+                        </div>
+                        <div class="form-24__entry">
+                            <label for="last_name">{{ __('Last name') }}</label>
+                            <input type="text" name="last_name" placeholder="{{ __('Last name') }}" />
+                        </div>
+                    </div>
+                    <label for="phone">{{ __('Phone number') }}</label>
+                    <div id="phone-fields">
+                        <select name="phone_code">
+                            <option value="1">🇺🇸 +1</option>
+                            <option value="84">🇻🇳 +84</option>
+                        </select>
+                        <input type="tel" name="phone" placeholder="{{ __('Phone number') }}" />
+                    </div>
+                    <label for="descr">{{ __('Description') }}</label>
+                    <input type="text" name="descr" placeholder="{{ __('Description') }}" />
+                    {{-- <label for="website">{{ __('Website') }}</label>
+                    <input type="text" name="website" placeholder="{{ __('Website') }}" /> --}}
+                    <label for="pricing">{{ __('Pricing').' ('.$currency_symbol.')' }}</label>
+                    <input type="number" name="pricing" step="0.01" placeholder="{{ __('Pricing') }}" />
+
+                    <!-- Business trade -->
+                    <label for="trade">{{ __('Trade') }}</label>
+                    <select name="trade">
+                        @foreach ($trades as $key => $value)
+                        <option value="{{ $key }}">{{ $value }}</option>
+                        @endforeach
+                    </select>
+
+
+                    <!-- Business location (loaded dynamically) -->
+                    <div class="form-24__row">
+                        <div class="form-24__entry">
+                            <label for="district">{{ __('District') }}</label>
+                            <select class="notranslate" name="district">
+                                @foreach ($districts as $district)
+                                <option value="{{ $district["code"] }}">{{ $district["name"] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-24__entry">
+                            <label for="ward">{{ __('Ward') }}</label>
+                            <select class="notranslate" name="ward">
+                                @foreach ($districts as $district)
+                                    @foreach ($district['ward'] as $ward)
+                                    <option @class(['inactive'=> $district !== $districts[0]])
+                                        x-district="{{ $district["code"] }}" value="{{ $ward["code"] }}">{{ $ward["name"] }}
+                                    </option>
+                                    @endforeach
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+
+                    <input onchange="validateSize(this)" type="file" name="image" />
+                    <p id="file-error">&nbsp;</p>
+
+                </div>
+
+                <input type="submit" value="{{ __('Submit') }}" />
+            </form>
+        </div>
+        <div id="existing-account-form" style="display:none">
+            <!-- Form elements for logging into an existing account -->
+            <form class="form-24" method="post" action="{{ route('login') }}">
+                @csrf
+
+                <label for="email">{{ __('Email') }}</label>
+                <input type="email" name="email" placeholder="{{ __('Enter email') }}" />
+                <label for="password">{{ __('Password') }}</label>
+                <input type="password" id="password" name="password" placeholder="{{ __('Create password') }}" />
+
+                <input type="submit" value="{{ __('Submit') }}" />
+            </form>
+        </div>
+    </section>
+</div>
 @endsection
