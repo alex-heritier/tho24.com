7ec788aa893d2e1b4f97b3af42d8efbcec35a599
alex <alex.heritier@gmail.com>
2023-09-28 16:44:53 +0700

    added currency_code to positions table

---
+++ database/migrations/2023_09_28_085808_add_currency_code_to_positions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('positions', function (Blueprint $table) {
+            $table->after('address', function (Blueprint $table) {
+                $table->string('currency_code', 8)->default('USD');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('positions', function (Blueprint $table) {
+            $table->dropColumn('currency_code');
+        });
+    }
+};
